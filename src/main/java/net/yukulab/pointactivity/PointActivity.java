2e068dc0e48daf511cabd5640306fa6252313674
turtton <top.gear7509@turtton.net>
2022-11-22 16:41:00 +0900

    :hammer: add mod name and logger value

--- src/main/java/net/yukulab/pointactivity/PointActivity.java
+++ src/main/java/net/yukulab/pointactivity/PointActivity.java
@@ -1,8 +1,13 @@
 package net.yukulab.pointactivity;
 
 import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 public class PointActivity implements ModInitializer {
+    public static String MOD_NAME = "pointactivity";
+    public static Logger LOGGER = LoggerFactory.getLogger(MOD_NAME);
     @Override
     public void onInitialize() {
 
