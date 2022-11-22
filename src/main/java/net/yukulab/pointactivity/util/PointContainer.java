dbf58bcc7f67a58e48df0399f2eabee3bb8e3a9a
turtton <top.gear7509@turtton.net>
2022-11-22 18:06:58 +0900

    :hammer: add functions for client side and add env checker

--- src/main/java/net/yukulab/pointactivity/util/PointContainer.java
+++ src/main/java/net/yukulab/pointactivity/util/PointContainer.java
@@ -1,26 +1,54 @@
 package net.yukulab.pointactivity.util;
 
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.yukulab.pointactivity.network.packet.play.UpdatePointS2CPacket;
+
+import java.time.Duration;
+import java.time.Instant;
 
 /**
  * 行動ポイントを保持するクラス
  */
 public class PointContainer {
+    @Environment(EnvType.SERVER)
     private final ServerPlayerEntity player;
     private int currentPoint;
 
+    @Environment(EnvType.CLIENT)
+    private int currentCombo;
+
+    @Environment(EnvType.CLIENT)
+    private Instant lastComboTime;
+
+    /**
+     * コンボの継続時間
+     * これは後々コンフィグとかで変更できるようにする
+     */
+    @Environment(EnvType.CLIENT)
+    private static final int continueTimeMillis = 1500;
+
+    @Environment(EnvType.SERVER)
     public PointContainer(ServerPlayerEntity player) {
         this.player = player;
     }
 
-    public void substractPoint(int amount) {
+    @Environment(EnvType.CLIENT)
+    public PointContainer() {
+        player = null;
+    }
+
+    public void subtractPoint(int amount) {
         if (currentPoint < amount) {
             currentPoint = 0;
         } else {
             currentPoint -= amount;
         }
-        sendUpdatePacket();
+        if (!isClientSide()) {
+            sendUpdatePacket();
+        }
     }
 
     public void setPoint(int point) {
@@ -31,8 +59,13 @@ public class PointContainer {
             return;
         }
 
-        currentPoint = point;
-        sendUpdatePacket();
+        if (isClientSide()) {
+            currentCombo = point;
+            lastComboTime = Instant.now();
+        } else {
+            currentPoint = point;
+            sendUpdatePacket();
+        }
     }
 
     public int getPoint() {
@@ -43,6 +76,20 @@ public class PointContainer {
         return currentPoint > 0;
     }
 
+    @Environment(EnvType.CLIENT)
+    public void tick() {
+        var now = Instant.now();
+        if (currentCombo != 0 && Duration.between(lastComboTime, now).toMillis() > continueTimeMillis) {
+            subtractPoint(currentCombo);
+            currentCombo = 0;
+        }
+    }
+
+    private boolean isClientSide() {
+        return FabricLoader.getInstance().getEnvironmentType() == EnvType.CLIENT;
+    }
+
+    @Environment(EnvType.SERVER)
     private void sendUpdatePacket() {
         UpdatePointS2CPacket.send(player, currentPoint);
     }
