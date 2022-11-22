7d209e1fe658d6494d0fa41bd76edb8d7029bb26
turtton <top.gear7509@turtton.net>
2022-11-22 22:17:01 +0900

    :recycle: refactor codes

--- src/main/java/net/yukulab/pointactivity/util/ConfigIO.java
+++ src/main/java/net/yukulab/pointactivity/util/ConfigIO.java
@@ -5,16 +5,25 @@ import com.google.gson.GsonBuilder;
 import net.fabricmc.loader.api.FabricLoader;
 import net.yukulab.pointactivity.PointActivity;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Optional;
 
 public class ConfigIO {
-    private static final Gson gson = new GsonBuilder().setPrettyPrinting().create();
-    private static final File configFile = new File(FabricLoader.getInstance().getConfigDir().toFile(), "PointActivity.json");
+    protected ConfigIO() {
+        throw new UnsupportedOperationException("Do not create this class instance");
+    }
+
+    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+    private static final File CONFIG_FILE =
+            new File(FabricLoader.getInstance().getConfigDir().toFile(), "PointActivity.json");
 
     public static <T> void writeConfig(T config) {
-        var json = gson.toJson(config);
-        try (var writer = new FileWriter(configFile, false)) {
+        var json = GSON.toJson(config);
+        try (var writer = new FileWriter(CONFIG_FILE, false)) {
             writer.write(json);
         } catch (IOException e) {
             PointActivity.LOGGER.error("Failed to write config", e);
@@ -22,8 +31,8 @@ public class ConfigIO {
     }
 
     public static <T> Optional<T> readConfig(Class<T> targetObject) {
-        try (var reader = new FileReader(configFile)) {
-            var config = gson.fromJson(reader, targetObject);
+        try (var reader = new FileReader(CONFIG_FILE)) {
+            var config = GSON.fromJson(reader, targetObject);
             return Optional.of(config);
         } catch (FileNotFoundException e) {
             PointActivity.LOGGER.warn("Config file not found. Load default data", e);
