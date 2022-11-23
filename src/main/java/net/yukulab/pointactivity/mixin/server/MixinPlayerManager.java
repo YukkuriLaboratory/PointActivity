ebebff4400397eaf3de4ae21ffdd74188e2badbf
turtton <top.gear7509@turtton.net>
2022-11-23 12:14:38 +0900

    :bug: fix point data send timing

--- src/main/java/net/yukulab/pointactivity/mixin/server/MixinPlayerManager.java
+++ src/main/java/net/yukulab/pointactivity/mixin/server/MixinPlayerManager.java
@@ -7,6 +7,7 @@ import net.minecraft.server.dedicated.MinecraftDedicatedServer;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.yukulab.pointactivity.PointActivity;
 import net.yukulab.pointactivity.network.packet.play.SendServerConfigBothPacket;
+import net.yukulab.pointactivity.network.packet.play.UpdatePointS2CPacket;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -32,13 +33,18 @@ public abstract class MixinPlayerManager {
             method = "onPlayerConnect",
             at = @At("RETURN")
     )
-    public void sendServerConfig(ClientConnection connection, ServerPlayerEntity player, CallbackInfo ci) {
+    public void sendServerData(ClientConnection connection, ServerPlayerEntity player, CallbackInfo ci) {
         if (connection.pointactivity$isModLoaded()) {
             if (getServer() instanceof MinecraftDedicatedServer dedicatedServer) {
                 SendServerConfigBothPacket.send(player, dedicatedServer.pointactivity$getServerConfig());
             } else {
                 PointActivity.LOGGER.warn("Unexpected error.", new RuntimeException("Server is not DedicatedServer"));
             }
+            player.pointactivity$getPointContainer().ifPresentOrElse(
+                    container -> UpdatePointS2CPacket.send(player, container.getPoint()),
+                    () ->
+                            PointActivity.LOGGER.warn("Unexpected error.", new RuntimeException("Container is null"))
+            );
         }
     }
 }
