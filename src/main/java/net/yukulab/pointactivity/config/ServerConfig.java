d6de2421e42dfef1cccd9e2bcb4c4688e7238521
turtton <top.gear7509@turtton.net>
2022-11-23 17:16:16 +0900

    :hammer: implement craft point consumer

--- src/main/java/net/yukulab/pointactivity/config/ServerConfig.java
+++ src/main/java/net/yukulab/pointactivity/config/ServerConfig.java
@@ -4,8 +4,8 @@ import com.google.gson.annotations.JsonAdapter;
 import marcono1234.gson.recordadapter.RecordTypeAdapterFactory;
 
 @JsonAdapter(RecordTypeAdapterFactory.class)
-public record ServerConfig(int moveHorizontalPointPer, int moveVerticalPointPer) {
+public record ServerConfig(int moveHorizontalPointPer, int moveVerticalPointPer, int craftPoint) {
     public static ServerConfig getAsDefault() {
-        return new ServerConfig(100, 250);
+        return new ServerConfig(100, 250, 10);
     }
 }
